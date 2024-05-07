f69ec89d41cfde6631e31ec6464e4272eaafe657
Hieu <anrayno200@gmail.com>
2024-05-07 09:56:10 +0700

    add schedule to fetch new data after 30 minutes

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import pandas
 from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 import pyodbc
+import schedule
 
-conn_str = (
+def job():
+    global courses_with_rate, users, subjects
+    conn_str = (
     r"DRIVER={ODBC Driver 17 for SQL Server};"
     r"Server=homelab-quy.duckdns.org,1433;"
     r"Database=es_mssql;"
@@ -12,43 +15,41 @@ conn_str = (
     r"UID=SA;"
     r"PWD=1q2w3E**;"
     r"MultipleActiveResultSets=Yes;"
-)
-try:
-    # Connect to the database
-    conn = pyodbc.connect(conn_str)
-    sql_query = "SELECT * FROM Course"
+    )
+    try:
+        # Connect to the database
+        conn = pyodbc.connect(conn_str)
+        sql_query = "SELECT * FROM Course"
 
-    # Perform operations here, such as executing SQL queries
+        # Perform operations here, such as executing SQL queries
 
-    # Don't forget to close the connection when done
-    # SQL query to retrieve data
-    courses_with_rate = pd.read_sql(sql_query, conn)
-    courses_with_rate.rename(columns={'Id': 'course_id'}, inplace=True)
-    courses_with_rate.rename(columns={'LearnerId': 'learner_id'}, inplace=True)
+        # Don't forget to close the connection when done
+        # SQL query to retrieve data
+        courses_with_rate = pd.read_sql(sql_query, conn)
+        courses_with_rate.rename(columns={'Id': 'course_id'}, inplace=True)
+        courses_with_rate.rename(columns={'LearnerId': 'learner_id'}, inplace=True)
 
-    sql_query = "SELECT * FROM Customer"
+        sql_query = "SELECT * FROM Customer"
 
-    # courses = pandas.read_csv('Courses.csv',usecols=[0, 1, 12] ,names=courses_cols,encoding='utf-8')
-    users =  pd.read_sql(sql_query, conn)
-    users.rename(columns={'Id': 'user_id'}, inplace=True)
+        # courses = pandas.read_csv('Courses.csv',usecols=[0, 1, 12] ,names=courses_cols,encoding='utf-8')
+        users =  pd.read_sql(sql_query, conn)
+        users.rename(columns={'Id': 'user_id'}, inplace=True)
 
-    sql_query = "SELECT * FROM Subject"
+        sql_query = "SELECT * FROM Subject"
 
-    # courses = pandas.read_csv('Courses.csv',usecols=[0, 1, 12] ,names=courses_cols,encoding='utf-8')
-    subjects =  pd.read_sql(sql_query, conn)
+        # courses = pandas.read_csv('Courses.csv',usecols=[0, 1, 12] ,names=courses_cols,encoding='utf-8')
+        subjects =  pd.read_sql(sql_query, conn)
 
-    conn.close()
-    print("Connection closed successfully.")
-    print("Courses \n")
-    print(courses_with_rate.info())
-    print("user \n")
-    print(users.head(5))
-    print("subject \n")
-    print(subjects.head(5))
-except Exception as e:
-    print("Error connecting to database:", e)
-
-
+        conn.close()
+        print("Connection closed successfully.")
+        print("Courses \n")
+        print(courses_with_rate.info())
+        print("user \n")
+        print(users.head(5))
+        print("subject \n")
+        print(subjects.head(5))
+    except Exception as e:
+        print("Error connecting to database:", e)
 
 def get_course_index_by_title(course_pivot, course_id):
     id = pd.to_numeric(course_id, errors='coerce', downcast='integer')
@@ -117,6 +118,8 @@ def get_dataframe_ratings_base(id):
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+job()
+schedule.every(30).minutes.do(job)
 
 @app.route("/get_recommend/<id>")
 def get_recommended_courses(id):
@@ -131,6 +134,15 @@ def hello_world():
 
 # app.run(debug=True,host="0.0.0.0",port=8001)
 
+def run_scheduler():
+    # Run the pending jobs once to execute the scheduled job for the first time
+    while True:
+        schedule.run_pending()
+        
 if __name__ == "__main__":
+    import threading
+    scheduler_thread = threading.Thread(target=run_scheduler)
+    scheduler_thread.start()
+
     from waitress import serve
     serve(app, host="0.0.0.0", port=8080)
